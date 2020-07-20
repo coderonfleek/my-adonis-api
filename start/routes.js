61240cfb5632e000c2ab68ef3f119c98b120dd7d
Fikayo <fik4christ@yahoo.com>
2020-07-20 09:26:22 +0100

    Initial Commit

---
+++ start/routes.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const Route = use("Route");
+
+Route.get("/", () => {
+  return { greeting: "Welcome to the Adonis API tutorial" };
+});
+
+Route.group(() => {
+  Route.post("create", "UserController.create");
+
+  Route.route("get", "UserController.fetch", ["GET", "POST"]);
+}).prefix("user");
